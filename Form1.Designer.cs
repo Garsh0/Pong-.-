cae099959f818c72569389589189b2b0a230d2d2
Garsh0 <73856919+Garsh0@users.noreply.github.com>
2021-09-27 22:08:32 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Pong__._
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rightPaddle = new System.Windows.Forms.PictureBox();
+            this.leftPaddle = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.paddleTimerUp = new System.Windows.Forms.Timer(this.components);
+            this.paddleTimerDown = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rightPaddle
+            // 
+            this.rightPaddle.AccessibleName = "rightPaddle";
+            this.rightPaddle.Image = ((System.Drawing.Image)(resources.GetObject("rightPaddle.Image")));
+            this.rightPaddle.Location = new System.Drawing.Point(714, 215);
+            this.rightPaddle.Name = "rightPaddle";
+            this.rightPaddle.Size = new System.Drawing.Size(13, 80);
+            this.rightPaddle.TabIndex = 0;
+            this.rightPaddle.TabStop = false;
+            // 
+            // leftPaddle
+            // 
+            this.leftPaddle.AccessibleName = "leftPaddle";
+            this.leftPaddle.Image = ((System.Drawing.Image)(resources.GetObject("leftPaddle.Image")));
+            this.leftPaddle.Location = new System.Drawing.Point(70, 215);
+            this.leftPaddle.Name = "leftPaddle";
+            this.leftPaddle.Size = new System.Drawing.Size(13, 80);
+            this.leftPaddle.TabIndex = 1;
+            this.leftPaddle.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.AccessibleName = "ball";
+            this.ball.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ball.Location = new System.Drawing.Point(400, 248);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(15, 15);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.startButton.Location = new System.Drawing.Point(331, 204);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(144, 40);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(70, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(657, 346);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // paddleTimerUp
+            // 
+            this.paddleTimerUp.Interval = 10;
+            this.paddleTimerUp.Tick += new System.EventHandler(this.paddleTimerUp_Tick);
+            // 
+            // paddleTimerDown
+            // 
+            this.paddleTimerDown.Interval = 10;
+            this.paddleTimerDown.Tick += new System.EventHandler(this.paddleTimerDown_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(308, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 53);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "PONG!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.leftPaddle);
+            this.Controls.Add(this.rightPaddle);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.rightPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox rightPaddle;
+        private System.Windows.Forms.PictureBox leftPaddle;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer paddleTimerUp;
+        private System.Windows.Forms.Timer paddleTimerDown;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong__._
+{
+    public partial class Form1 : Form
+    {
+        // global variables
+        public string direction;
+        public int speed = 5;
+        public bool cont = true;
+        public string angleDirection = "none";
+        public int paddleAngle = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //ON START BUTTON
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            direction = "right";
+            cont = true;
+            angleDirection = "none";
+            paddleAngle = 0;
+            ball.Location = new Point(400, 248);
+            rightPaddle.Location = new Point(714, 215);
+            leftPaddle.Location = new Point(70, 215);
+            timer.Enabled = true;
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(cont == false)
+            {
+                timer.Enabled = false;
+            }
+
+            if (direction.Equals("right"))
+            {
+                if (ball.Location.X <= (rightPaddle.Location.X - 14))
+                {
+                    //check if it hits bounds
+                    if (ball.Location.Y <= 81)
+                    {
+                        angleDirection = "down";
+                    }
+                    if (ball.Location.Y >= 412)
+                    {
+                        angleDirection = "up";
+                    }
+
+
+                    if (angleDirection.Equals("up"))
+                    {
+                        ball.Location = new Point(ball.Location.X + speed, ball.Location.Y + paddleAngle);
+                    }
+                    else if(angleDirection.Equals("down"))
+                    {
+                        ball.Location = new Point(ball.Location.X + speed, ball.Location.Y - paddleAngle);
+                    }
+                    else
+                    {
+                        ball.Location = new Point(ball.Location.X + speed, ball.Location.Y);
+                    }
+                    //ball.Location = new Point(ball.Location.X + speed, ball.Location.Y);
+                }
+
+                else if ((ball.Location.Y >= rightPaddle.Location.Y - 15) && (ball.Location.Y <= rightPaddle.Location.Y + 90))
+                {
+                    direction = "left";
+                    ballAngleResult(ball.Location.Y, rightPaddle.Location.Y);
+                }
+                else
+                {
+                    cont = false;
+                    startButton.Visible = true;
+                }
+            }
+
+            else if (direction.Equals("left"))
+            {
+                if (ball.Location.X >= (leftPaddle.Location.X + 12))
+                {
+                    //check if it hits bounds
+                    if(ball.Location.Y <= 81)
+                    {
+                        angleDirection = "down";
+                    }
+                    if(ball.Location.Y >= 412)
+                    {
+                        angleDirection = "up";
+                    }
+
+
+                    if (angleDirection.Equals("up"))
+                    {
+                        ball.Location = new Point(ball.Location.X - speed, ball.Location.Y + paddleAngle);
+                    }
+                    else if (angleDirection.Equals("down"))
+                    {
+                        ball.Location = new Point(ball.Location.X - speed, ball.Location.Y - paddleAngle);
+                    }
+                    else
+                    {
+                        ball.Location = new Point(ball.Location.X - speed, ball.Location.Y);
+                    }
+                    //ball.Location = new Point(ball.Location.X - speed, ball.Location.Y);
+                }
+
+                else if ((ball.Location.Y >= leftPaddle.Location.Y - 15) && (ball.Location.Y <= leftPaddle.Location.Y + 90))
+                {
+                    direction = "right";
+                    ballAngleResult(ball.Location.Y, leftPaddle.Location.Y);
+                }
+                else
+                {
+                    cont = false;
+                    startButton.Visible = true;
+                }
+            }
+
+            //left paddle moves to ball
+            if (angleDirection.Equals("up"))
+            {
+                if (leftPaddle.Location.Y > 84 + speed)
+                {
+                    leftPaddle.Location = new Point(leftPaddle.Location.X, leftPaddle.Location.Y - (speed - 2));
+                }
+            }
+            else if (angleDirection.Equals("down"))
+            {
+                if (leftPaddle.Location.Y < 350 - speed)
+                {
+                    leftPaddle.Location = new Point(leftPaddle.Location.X, leftPaddle.Location.Y + (speed - 2));
+                }
+            }
+        }
+
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Up)
+            {
+                paddleTimerUp.Enabled = true;
+            }
+            if (e.KeyData == Keys.Down)
+            {
+                paddleTimerDown.Enabled = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Up)
+            {
+                paddleTimerUp.Enabled = false;
+            }
+            if (e.KeyData == Keys.Down)
+            {
+                paddleTimerDown.Enabled = false;
+            }
+        }
+
+        private void paddleTimerUp_Tick(object sender, EventArgs e)
+        {
+            if(cont == true)
+            {
+                if (rightPaddle.Location.Y > 84 + speed)
+                {
+                    rightPaddle.Location = new Point(rightPaddle.Location.X, rightPaddle.Location.Y - (speed - 2));
+                }
+            }
+            
+        }
+
+        private void paddleTimerDown_Tick(object sender, EventArgs e)
+        {
+            if(cont == true)
+            {
+                if (rightPaddle.Location.Y < 350 - speed)
+                {
+                    rightPaddle.Location = new Point(rightPaddle.Location.X, rightPaddle.Location.Y + (speed - 2));
+                }
+            }
+        }
+
+        
+        //determines angle of ball when hit
+        private void ballAngleResult(int ballY, int paddleY)
+        {
+            int relativeY = -1 * (paddleY - (ballY + 7));
+
+            if (relativeY < 39)
+            {
+                angleDirection = "up";
+                relativeY = (relativeY - 40) / 3;
+                if(relativeY < -3)
+                {
+                    relativeY = -3;
+                }
+                if (relativeY > 3)
+                {
+                    relativeY = -3;
+                }
+            }
+            else if (relativeY > 41)
+            {
+                angleDirection = "down";
+                relativeY = (80 - (40 + relativeY)) / 3;
+                if (relativeY < -3)
+                {
+                    relativeY = -3;
+                }
+                if (relativeY > 3)
+                {
+                    relativeY = -3;
+                }
+            }
+            else
+            {
+                angleDirection = "none";
+                relativeY = 0;
+            }
+
+            paddleAngle = relativeY;
+        }
+    }
+}
